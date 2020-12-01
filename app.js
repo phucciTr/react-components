ec7a56ccda837a3f5448247b6a7e221f0f9c6b3e
phucciTr <miabam26@gmail.com>
2020-12-01 14:35:26 -0800

    Complete React Components

--- app.js
+++ app.js
@@ -1,1 +1,110 @@
-// TODO
+
+// // Original
+// // React component
+// var Bread = () => (
+//   <li> bread </li>
+// );
+//
+// // React component
+// var Butter = () => (
+//   <li> butter </li>
+// );
+//
+// // Nested React components
+// var GroceryList = () => (
+//   <ul>
+//    <Bread />               {/* instance of Bread  */}
+//    <Butter />              {/* instance of Butter  */}
+//   </ul>
+//  );
+//
+// // Render GroceryList component into the DOM
+// ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+
+
+// // Refactor.1
+// var GroceryList = (props) => (
+//   <ul>
+//     {GroceryListItem(props.items[0])}
+//     {GroceryListItem(props.items[1])}
+//   </ul>
+// );
+
+// var GroceryListItem = (content) => (
+//   <li>{content}</li>
+// );
+
+// ReactDOM.render(<GroceryList items={['bread', 'butter']} />, document.getElementById("app"));
+
+
+// // Refactor.2
+// var GroceryList = (props) => (
+//   <ul>
+//     {props.items.map(current =>
+//       <GroceryListItem item={current} />
+//     )}
+//   </ul>
+// );
+
+// class GroceryListItem extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return (
+//     <li>{this.props.item}</li>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<GroceryList items={['bread', 'butter']} />, document.getElementById("app"));
+
+
+// Refactor.3
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(current =>
+      <GroceryListItem item={current} />
+    )}
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // current state of compnent
+    this.state = {
+      done: false
+    };
+  }
+
+  // re-render componenet when gets clicked
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    // conditional style object literal for inline styling on clicked component
+      // snake-cased css props become camelCased
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+ReactDOM.render(<GroceryList items={['bread', 'butter']} />, document.getElementById("app"));
+
+
+
+
+
